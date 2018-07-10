163ab901e5ed1d26bb292e6c13fe0ec69367dc76
ikechukwuhenry <ikechukwuhenry@ymail.com>
2018-07-10 10:32:38 +0100

    Initial Commit

---
+++ Circule_Animation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Circule_Animation
+//
+//  Created by Ikechukwu Michael on 10/07/2018.
+//  Copyright © 2018 Anyskills.co.uk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
